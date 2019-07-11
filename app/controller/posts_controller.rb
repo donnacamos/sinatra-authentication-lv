cef7b5e4345397c77c5a92c53806b4ca4b872e6c
Donna Amos <donnacamos.developer@gmail.com>
2019-07-11 01:09:30 +0000

    posts_controller

--- app/controller/posts_controller.rb
+++ app/controller/posts_controller.rb
@@ -1,6 +1,15 @@
 class PostsController < ApplicationController 
   
   get '/posts' do 
-    "You are logged in as #{session[:email]}" 
+    "A list of publicly available posts." 
+  end 
+  
+  get '/posts/new' 
+  # checking to see if they're logged in
+    if session[:email].empty? 
+      redirect '/login' # redirecting if they aren't 
+    else 
+      "A new post form" # rendering if they are 
+    end 
   end 
 end 
