55f40b8db7415de3bea10518dd6727b67c686fdc
Donna Amos <donnacamos.developer@gmail.com>
2019-07-11 02:54:23 +0000

    update

--- app/controller/users_controller.rb
+++ app/controller/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController 
-  has_secure_password 
+  get 'signup' do 
+    erb :"users/new" 
+  end 
+  
+  post 'user/new' do 
+    @user = User.new 
+    @user.email = params[:email] 
+    @user.password = params[:password] 
+    if @user.save 
+      redirect '/login' 
+    else 
+      erb :"users/new"  
+    end 
+  end 
 end 
