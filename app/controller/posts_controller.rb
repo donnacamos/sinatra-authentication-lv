54d47cd96d24e3e8a567008b0bd7d7a7fd7c8643
Donna Amos <donnacamos.developer@gmail.com>
2019-07-11 01:27:43 +0000

    update

--- app/controller/posts_controller.rb
+++ app/controller/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   
   get '/posts/new' 
   # checking to see if they're logged in
-    if !session[:email] 
+    if !logged_in? 
       redirect '/login' # redirecting if they aren't 
     else 
       "A new post form" # rendering if they are 
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   end 
   
   get '/posts/:id/edit' do
-     if !session[:email] 
+     if !logged_in?
       redirect '/login' # redirecting if they aren't 
     else 
       "An edit post form" # rendering if they are 
